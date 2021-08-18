0f24a38a7ac4c10c46561cd3a3cee70c5471cdc6
toBeOfUse <mitchjacov@gmail.com>
2021-08-18 01:04:21 -0400

    add subtitles and fonts

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const player = new Plyr("#video-player", {
     title: "JOJO",
-    debug: true,
     controls: [
         "play",
         "progress",
@@ -9,5 +8,16 @@ const player = new Plyr("#video-player", {
         "volume",
         "captions",
         "fullscreen",
+        "settings"
     ],
+    settings: ["captions"]
 });
+
+player.on("canplaythrough", () => console.log("client can play through"));
+player.on("playing", console.log("client has started playing"));
+player.on("seeking", () => console.log("client is seeking"));
+player.on("seeked", () => {
+    console.log("client has seeked");
+    console.log("current time is", player.currentTime);
+});
+player.on("pause", () => console.log("client has paused"));
