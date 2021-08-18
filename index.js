fb8dce7d3b81ef5615180531dcd4e3fead7c9659
toBeOfUse <mitchjacov@gmail.com>
2021-08-18 03:36:07 -0400

    add loading spinner while images load

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import "normalize.css";
-import "./index.css"
+import "./index.css";
 import "./fonts/fonts.css";
 import Plyr from "plyr";
 import "./node_modules/plyr/dist/plyr.css";
+
+// create playlist
 
 const playlist = [
     {
@@ -14,37 +16,68 @@ const playlist = [
 
 let currentPlaylistItem = 0;
 
-const player = new Plyr("#video-player", {
-    title: "JOJO",
-    controls: [
-        "play",
-        "progress",
-        "current-time",
-        "mute",
-        "volume",
-        "captions",
-        "fullscreen",
-        "settings"
-    ],
-    settings: ["captions"],
-    captions: { active: true },
-    invertTime: false,
-    ratio: "16:9",
-});
+let player = undefined;
 
-player.source = playlist[currentPlaylistItem];
+function initVideoPlayer() {
+    player = new Plyr("#video-player", {
+        title: "JOJO",
+        controls: [
+            "play",
+            "progress",
+            "current-time",
+            "mute",
+            "volume",
+            "captions",
+            "fullscreen",
+            "settings"
+        ],
+        settings: ["captions"],
+        captions: { active: true },
+        invertTime: false,
+        ratio: "16:9",
+    });
 
-player.on("canplaythrough", () => console.log("client can play through"));
-player.on("playing", () => {
-    console.log("client has started playing")
-});
-player.on("seeking", () => console.log("client is seeking"));
-player.on("seeked", () => {
-    console.log("client has seeked");
-    console.log("current time is", player.currentTime);
-});
-player.on("pause", () => console.log("client has paused"));
+    // log video events
 
+    player.on("canplaythrough", () => console.log("client can play through"));
+    player.on("playing", () => {
+        console.log("client has started playing")
+    });
+    player.on("seeking", () => console.log("client is seeking"));
+    player.on("seeked", () => {
+        console.log("client has seeked");
+        console.log("current time is", player.currentTime);
+    });
+    player.on("pause", () => console.log("client has paused"));
+
+    // give the player something to play
+    player.source = playlist[currentPlaylistItem];
+}
+
+// remove the loading spinner and create the video player once all the images have shown up
+let importantImages = 0;
+let loadedImages = 0;
+function checkImageCompletion() {
+    if (loadedImages == importantImages) {
+        document.querySelector("#initial-loading-spinner").remove();
+        document.querySelector("#container-container").style.display = "initial";
+        initVideoPlayer();
+    }
+}
+for (const img of Array.from(document.querySelectorAll(".wait-for-load"))) {
+    importantImages += 1;
+    if (img.complete) {
+        loadedImages += 1;
+        checkImageCompletion();
+    } else {
+        img.addEventListener("load", () => {
+            loadedImages += 1;
+            checkImageCompletion();
+        });
+    }
+}
+
+// set up playlist interactivity
 const next = document.querySelector("#next-button");
 const prev = document.querySelector("#prev-button");
 
