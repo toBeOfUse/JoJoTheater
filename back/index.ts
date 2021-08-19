1af4cb9de1ec83782840d451c5e372576878f63d
toBeOfUse <mitchjacov@gmail.com>
2021-08-18 20:50:55 -0400

    set up new node server to bundle and serve static files

--- back/index.ts
+++ back/index.ts
@@ -1,1 +1,28 @@
-console.log("hello world");
+import webpackConfig from "../webpack.config";
+import webpack from 'webpack';
+const webpacker = webpack(webpackConfig);
+webpacker.watch(
+    {
+        aggregateTimeout: 300,
+    },
+    (err, stats) => {
+        if (err || stats?.hasErrors()) {
+            console.log("webpack error");
+            if (err) {
+                console.error(err.stack || err);
+            }
+        }
+        console.log(
+            stats?.toString({
+                colors: true,
+                chunks: false,
+            })
+        );
+    }
+);
+
+const connect = require("connect");
+const serveStatic = require("serve-static");
+connect()
+    .use(serveStatic("./dist/"))
+    .listen(8080, () => console.log("Server running on 8080..."));
--- webpack.config.ts
+++ webpack.config.ts
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-module.exports = {
+export default {
     entry: './front/index.js',
     output: {
         filename: 'main.[contenthash].js',
