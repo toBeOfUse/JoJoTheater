552d0c0a94e28ca501295efe05ff7af512080929
toBeOfUse <mitchjacov@gmail.com>
2021-11-07 03:45:52 -0500

    update playlist

--- back/playlist.ts
+++ back/playlist.ts
@@ -4,19 +4,39 @@ interface Video {
     size?: number;
     title: string;
     provider?: string;
+    captions: boolean;
 }
 
 const playlist: Video[] = [
     {
-        src: "/videos/catra.mp4",
-        title: "Catra Doesn't Like Adora",
-        type: "video/mp4",
-        size: 720,
+        src: "GU4DJf2_jqE",
+        provider: "youtube",
+        title: "《双镜 Couple of Mirrors》EP01：3，2，1",
+        captions: true,
     },
     {
-        src: "sYyu5vbwvbA",
+        src: "8zqUunbOsoQ",
         provider: "youtube",
-        title: "Moves",
+        title: "《双镜 Couple of Mirrors》EP02: In the cold rainy night",
+        captions: true,
+    },
+    {
+        src: "SCp4a42sdWc",
+        provider: "youtube",
+        title: "《双镜 Couple of Mirrors》EP03: When the gun is fired",
+        captions: true,
+    },
+    {
+        src: "6btkPmu8j9M",
+        provider: "youtube",
+        title: "《双镜 Couple of Mirrors》EP04: Welcome to My World",
+        captions: true,
+    },
+    {
+        src: "Qx896VPc0LM",
+        provider: "youtube",
+        title: "《双镜 Couple of Mirrors》EP05: The Scene of the Third Crime",
+        captions: true,
     },
 ];
 
