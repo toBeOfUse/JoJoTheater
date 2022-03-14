8da4cf067f59888125bca7dcaa81a28f441d15b6
toBeOfUse <comingaflame@gmail.com>
2022-03-14 11:07:24 -0400

    add "names" table to keep track of users' names

---
+++ back/db/migrations/20220313020109_add_names.ts
@@ -0,0 +1,16 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable("names", (table) => {
+        table.increments();
+        table.string("name").notNullable();
+        table.string("nameType").notNullable();
+        table.integer("user").references("id").inTable("users").index();
+        table.timestamp("createdAt").notNullable();
+        table.boolean("latest").notNullable().defaultTo(true);
+    });
+}
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTableIfExists("names");
+}
